89be9799ad988cb2bda2c0b7e19d6e77dc0b6c5b
Kyle <kyle.fetterhoff@gmail.com>
2020-02-27 19:27:41 -0500

    Add Particle class

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,29 @@
 // Init Materalize JS Components
 M.AutoInit();
 
+let p;
+
 function setup() {
   var canvas = createCanvas(window.innerWidth, window.innerHeight);
+
+  p = new Particle();
 
   canvas.parent("home");
 }
 
-function draw() {}
+function draw() {
+  p.draw();
+}
+
+class Particle {
+  constructor() {
+    this.pos = createVector(random(width), random(height));
+    this.size = 10;
+  }
+
+  draw() {
+    noStroke();
+    fill("rgba(0, 0, 0, 0.5)");
+    circle(this.pos.x, this.pos.y, this.size);
+  }
+}
