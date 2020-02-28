afb6510dac7fb87fdbf3ea8ab811458ff25e5916
Kyle <kyle.fetterhoff@gmail.com>
2020-02-27 22:15:18 -0500

    Make carousel swipable

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ M.AutoInit();
 
 document.addEventListener("DOMContentLoaded", function() {
   var elems = document.querySelectorAll(".carousel");
-  M.Carousel.init(elems, { indicators: true });
+  M.Carousel.init(elems, { indicators: true, fullWidth: true });
 });
 
 // Particle System
