00c9035b823b0e387af234baa6347596c7601c00
Kyle <kyle.fetterhoff@gmail.com>
2020-02-27 19:20:11 -0500

    Add initial canvas particle effect

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,10 @@
+// Init Materalize JS Components
 M.AutoInit();
+
+function setup() {
+  var canvas = createCanvas(window.innerWidth, window.innerHeight);
+
+  canvas.parent("home");
+}
+
+function draw() {}
